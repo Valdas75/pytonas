7ef9f2445186a6125ff9626e89f72ea6c72012bd
Valdas75 <Valdas75>
2019-03-27 08:34:41 +0200

    valdas.py

---
+++ valdas.py
@@ -0,0 +1,56 @@
+print ("Valdo namu darbas")
+
+x = (2)
+y = (15)
+print (x+y)
+
+x = (2)
+y = (15)
+print (x*y)
+
+x = (2)
+y = (15)
+print (y - x)
+
+x = (5)
+y = (50)
+print (y/x)
+
+x =(100)
+y = (30)
+print (x/y)
+
+a = 90
+b = 95
+if b > a:
+   print("b yra didesnis uz a")
+
+a = 100
+b = 30
+if b > a:
+   print("b yra didesnis uz a")
+else:
+   print("b nera didesnis uz a")
+
+a = 20
+b = 33
+
+print("a") if a > b else print("b")
+
+print ("Valdas"*4)
+
+test =20
+test =test+20
+print(test)
+
+oras = 'Ziema'
+
+if oras == 'Ziema':
+   print ('Dabar salta geriau niekur nejeiti!')
+
+print (10!=11)
+print (10!=10)
+print (10>5)
+print (5>10)
+print (50//3)
+print (input('koks tavo vardas?:'))
